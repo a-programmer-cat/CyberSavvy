543c84454cfbc5f6dc153a1dbf8a5668510ee6c9
a-programmer-cat <sakobaki20@gmail.com>
2025-10-24 20:33:00 +0800

    animation

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import TextType from './TextType';
+import DecryptedText from './DecryptedText';
+import TrueFocus from './TrueFocus';
 
 interface IPData {
   ip?: string;
@@ -29,38 +32,59 @@ export default function IntroScreen({ onFinish }: { onFinish: () => void }) {
     // 控制步骤时间
     const timers = [
       setTimeout(() => setStep(1), 5000),
-      setTimeout(() => setStep(2), 10000),
-      setTimeout(() => setStep(3), 15000),
+      setTimeout(() => setStep(2), 15000),
+      setTimeout(() => setStep(3), 20000),
     ];
     return () => timers.forEach(clearTimeout);
   }, []);
+
+  const infoTexts = 
+    `IP Address: ${ipData?.ip ?? "Loading..."}\n` +
+    `Location: ${ipData?.city
+      ? `${ipData.city}, ${ipData.region}, ${ipData.country_name}`
+      : "Detecting..."}\n` +
+    `ISP: ${ipData?.org ?? "Detecting..."}\n` +
+    `Device: ${deviceInfo ?? "Unknown"}` ;
+
 
   return (
     <div className="fixed inset-0 z-[200] h-screen w-full flex flex-col items-center justify-center bg-black text-white font-mono overflow-hidden">
       {/* Step 0: 数据分析提示 */}
       {step === 0 && (
-        <div className="text-xl">Analyzing your data...</div>
+        <div className="text-3xl font-bold text-left space-y-2">
+          <DecryptedText
+            text="Analyzing your data..."
+            speed={100}
+            maxIterations={20}
+            animateOn="view"
+            revealDirection="center"
+          />
+        </div>
       )}
 
       {/* Step 1: 显示真实 IP 信息 */}
       {step === 1 && (
-        <div className="text-green-400 text-left space-y-2">
-          <p>IP Address: {ipData.ip || "Loading..."}</p>
-          <p>
-            Location:{" "}
-            {ipData.city
-              ? `${ipData.city}, ${ipData.region}, ${ipData.country_name}`
-              : "Detecting..."}
-          </p>
-          <p>ISP: {ipData.org || "Detecting..."}</p>
-          <p>Device: {deviceInfo}</p>
+        <div className="text-3xl font-bold text-green-400 text-left space-y-2">
+          <TextType
+            text={infoTexts}
+            typingSpeed={50}
+            pauseDuration={1500}
+            showCursor={true}
+            cursorCharacter="|"
+          />
         </div>
       )}
 
       {/* Step 2: 警告提示 */}
       {step === 2 && (
-        <div className="text-2xl text-red-500 font-bold text-center">
-          You just shared this with a website.
+        <div className="text-3xl text-red-500 font-bold text-center">
+          <TextType
+            text={["You just shared this with a website."]}
+            typingSpeed={50}
+            pauseDuration={1500}
+            showCursor={true}
+            cursorCharacter="|"
+          />
         </div>
       )}
 
